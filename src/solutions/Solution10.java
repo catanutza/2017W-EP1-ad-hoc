2eb25413bb996854f075f709da1532060b2386be
catanutza <35463889+catanutza@users.noreply.github.com>
2018-01-15 18:22:13 +0100

    Update Solution10.java

--- src/solutions/Solution10.java
+++ src/solutions/Solution10.java
@@ -4,7 +4,19 @@
  */
 public class Solution10 {
     public static void main(String[] args) {
-
+       StdDraw.setCanvasSize(500,500);
+       StdDraw.setXscale(0,500);
+       StdDraw.setYscale(0,500);
+       int rad=240, x=250, y=250, r=255, g=255, b=255;
+       for(int i=1; i<=10; i++)
+       {
+           StdDraw.setPenColor(r,g,b);
+           StdDraw.filledCircle(x,y,rad);
+           rad-=20;
+           r=255/(i);
+           g=255/(i);
+           b=255/(i);
+       }
     }
 }
 
