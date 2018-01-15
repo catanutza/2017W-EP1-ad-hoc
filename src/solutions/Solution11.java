78b43b44816aac72ed4a95960fd5e62b48fbea6f
catanutza <35463889+catanutza@users.noreply.github.com>
2018-01-15 18:29:30 +0100

    Update Solution11.java

--- src/solutions/Solution11.java
+++ src/solutions/Solution11.java
@@ -7,6 +7,18 @@
  */
 public class Solution11 {
     public static void main(String[] args) {
-
+        boolean x=true;
+       for(int i=0; i<3; i++){
+           System.out.println("");
+           System.out.print("| ");
+           for(int j=0; j<3; j++){
+               if(x)
+                   System.out.print("X ");
+               else
+                   System.out.print("O ");
+               x=x==true?false:true;
+               System.out.print("| ");
+           }
+       }
     }
 }
